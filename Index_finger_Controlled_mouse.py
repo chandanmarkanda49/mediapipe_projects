6ee0f11f385e9a9b4650b62d174cf52ddce3d0c7
CHANDAN MARKANDA <chandanmarkanda49@gmail.com>
2026-01-26 18:11:16 +0530

    Enhance mouse control with click and scroll features
    Added functionality for single and double clicks based on thumb and index finger distance. Implemented scrolling feature using index finger position.

--- Index_finger_Controlled_mouse.py
+++ Index_finger_Controlled_mouse.py
@@ -1,7 +1,9 @@
+import time
 import cv2        #importing all libraries needed
 import mediapipe as mp 
 import pyautogui
 import numpy as np 
+import math
 
 mp_hands = mp.solutions.hands  
 mp_drawing = mp.solutions.drawing_utils
@@ -9,8 +11,10 @@ hands = mp_hands.Hands(min_detection_confidence = 0.7,
                        min_tracking_confidence = 0.7   , 
                         )
 
+
 cap = cv2.VideoCapture(0) # 0 means the first webcame option available
 screen_width , screen_height = pyautogui.size()
+
 
 
 if not cap.isOpened():
@@ -34,9 +38,57 @@ while cap.isOpened() :   #main loop in which logic is written ~= void loop
             y = int(index_finger_tip.y * h)   #it returns the pixels value of the screen matlab screen kr pixels ke beech ka distance
             screen_x = np.interp(x , [0,w], [0,screen_width])
             screen_y = np.interp(y , [0,h], [0,screen_height])
-
-            pyautogui.moveTo(screen_x , screen_y , duration = 0.01) #method to move mouse
+          
             mp_drawing.draw_landmarks(frame , hand_landmarks , mp_hands.HAND_CONNECTIONS) #draws the landmarks on frame 
+
+            fingers = [
+                1 if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip-2].y else 0 
+                for tip in [8, 12 , 16 , 20 ]
+             ]
+
+            # distace b/w thumb tip and index finger tip 
+            click_times = []
+            thumb_tip = hand_landmarks.landmark[4]
+            dist = math.hypot(thumb_tip.x - index_finger_tip.x , thumb_tip.y - index_finger_tip.y)
+            freeze_cursor = False  #by default iss flag ko false rkh ke baad me true hoga to koi condition fulfill krvaynge
+            if dist < 0.04 :
+                if not freeze_cursor :
+                    freeze_cursor = True
+                    print("state :" , freeze_cursor) #check krr rha hu iss baar logic chal rha hai ya nhi  
+                    click_times.append(time.time())
+                    # double click check krr rhe hai 
+                    if len(click_times) >= 2 and click_times[-1] - click_times[-2] < 0.4 :
+                        pyautogui.doubleClick()
+                        cv2.putText(frame, "DOUBLE CLICK" , (10, 50), cv2.FONT_HERSHEY_PLAIN , 1 ,(0,255, 255), 2)
+                        # ye line frame prr print  hoti hai ( pixels  , font style , font scale , color code for rgb ,pixel thickness)
+                        click_times = []
+                        
+                    else:
+                        pyautogui.click()
+                        cv2.putText(frame, "SINGLE CLICK" , (10, 50), cv2.FONT_HERSHEY_PLAIN , 1 ,(0,255, 255, 0), 2)
+            else:
+             if freeze_cursor : 
+                 time.sleep(0.01) #thodi der ruk ke freeze cursor flag ko false krr dega  
+                 freeze_cursor = False
+
+            if not freeze_cursor :
+             pyautogui.moveTo(screen_x , screen_y , duration = 0.05) #method to move mouse
+            
+            ######  SCROLL FUNCTION #######
+            # scroll mode
+            scroll_mode = False
+            if sum(fingers) == 4 :
+                scroll_mode = True
+            else :
+                scroll_mode = False
+            if scroll_mode :
+                if index_finger_tip.y < 0.4 :
+                    pyautogui.scroll(60) #scroll krega upward 60 pixels ko , pyautogui mai sab code hai already , zyada dimaag lgana hi nhi hai 
+                    cv2.putText(frame, "Scrolling up" , (10, 50), cv2.FONT_HERSHEY_PLAIN , 1 ,(0,255,  0), 2)
+                    
+                elif index_finger_tip.y > 0.6 :
+                    pyautogui.scroll(-60)
+                    cv2.putText(frame, "Scroll down" , (10, 50), cv2.FONT_HERSHEY_PLAIN , 1 ,(0,0, 255,), 2)
 
     cv2.imshow("webcam_feed" , frame)   
 
